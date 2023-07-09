80b0ed3f434028e9579e2734d537801283ebd24a
XanderJ312 <xanderj312@mail.ru>
2023-07-09 23:12:00 +0200

    add city model and update response

--- backend/loginPage/models.py
+++ backend/loginPage/models.py
@@ -1,8 +1,9 @@
 from django.db import models 
 from django.core.validators import MaxValueValidator
  
-class Country(models.Model): 
+class country(models.Model): 
     CountryName = models.CharField(max_length=50)
+    flag_photo = models.CharField(max_length=255)
     NuclearTechnology = models.BooleanField()
     NuclearRockets = models.IntegerField(validators=[MaxValueValidator(20)])
     Budget = models.IntegerField(validators=[MaxValueValidator(10000)])
@@ -13,4 +14,25 @@ class Country(models.Model):
 
     class Meta:
         verbose_name_plural='Сountry'
-        db_table = 'сountry'
+        db_table = 'country'
+
+    def __str__(self):
+        return self.CountryName
+
+class city(models.Model):
+    city_name = models.CharField(max_length=50)
+    photo = models.CharField(max_length=250);
+    live_level = models.IntegerField()
+    progress = models.IntegerField()
+    profit = models.IntegerField()
+    shield = models.BooleanField() 
+    state = models.BooleanField()
+    country = models.ForeignKey(country, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural='City'
+        db_table = 'city'
+
+    def __str__(self):
+        return self.city_name
+    
