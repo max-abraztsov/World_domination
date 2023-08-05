c462226f59af0ae7317b8145e574cf02072a12a9
XanderJ312 <xanderj312@mail.ru>
2023-08-05 13:57:14 +0200

    Fixed another problem with duplicate sanctions

--- backend/round/views.py
+++ backend/round/views.py
@@ -40,9 +40,9 @@ def forms_check(request_data_show):
 
 
 def calculations(request_data):
-    print("Start calculations")
     print(request_data)
     country_name = request_data.get('country')
+    print(str(country_name) + ": start calculations")
     nuclear_technology = request_data.get('nuclear_technology')
     nuclear_rockets = request_data.get('rocket_order')
     donate_data = request_data.get('donate')
@@ -54,10 +54,11 @@ def calculations(request_data):
     Country = country.objects.get(CountryName=country_name)
     User = user.objects.get(country_id=Country.id)
     Ecology = ecology.objects.get(round=Country.Round)
+    
     Session = session.objects.get(id=1)
     Session.forms_count += 1
     Session.save()
-    print("Session.forms_count is up +1")
+    print(str(country_name) + ": session.forms_count is up +1")
 
     #add to database(attacked_cities) all cities under attack =================================================
     attacked_cities_list = []
@@ -79,7 +80,6 @@ def calculations(request_data):
 
     cost = 0
 
-    
     #send request to the app "donate" ========================================================================
     
     if donate_data['from'] != '' and donate_data['to'] != '':
@@ -88,23 +88,18 @@ def calculations(request_data):
         cost = response_data.get('budget')
     
     
-
     # add sanctions from this country =========================================================================
-    
     for enemy in enemies:
         if enemy['sanctions'] == True:
             country_under_sanction = country.objects.filter(CountryName=enemy['country']).values('id')
-            if_sanction_exist = sanction(sanctionFrom_id = Country.id, sanctionFor_id = country_under_sanction[0]['id'])
-            if if_sanction_exist == None:
+            if_sanction_exist = sanction.objects.filter(Q(sanctionFrom_id=Country.id) & Q(sanctionFor_id=country_under_sanction[0]['id'])).exists()
+            if if_sanction_exist == False:
                 new_sanction = sanction(sanctionFrom_id = Country.id, sanctionFor_id = country_under_sanction[0]['id'])
                 new_sanction.save()
-                print("Add sanctions")
+                print(str(country_name) + ": add sanctions on " + str(enemy['country']))
     
-                
-            
-            
-    #
-    
+                 
+    #update info about all cities in country (shields, progress, live level and profit)
     sum_profit = 0
     for one_request_city in cities:
         City = city.objects.get(country_id=Country.id, state=True, city_name=one_request_city['city_name'])
@@ -114,59 +109,59 @@ def calculations(request_data):
                 response_data = forms_check(request_data_show)
                 return response_data
             City.shield = True
-            print("add shield for city " + str(City.city_name))
+            print(str(country_name) + ": add shield for city " + str(City.city_name))
         if one_request_city['develop'] == True:
             cost += 150
             if cost > Country.Budget:
                 response_data = forms_check(request_data_show)
                 return response_data
             City.progress += 8
-            print("Upgrade progress to " + str(City.city_name))
+            print(str(country_name) + ": upgrade progress to " + str(City.city_name))
             City.live_level = (Ecology.level * City.progress)/100
-            print("Upgrade live level to " + str(City.city_name))
+            print(str(country_name) + ": upgrade live level to " + str(City.city_name))
             City.profit = City.live_level*3
-            print("Upgrade profit to " + str(City.city_name))
+            print(str(country_name) + "upgrade profit to " + str(City.city_name))
         sum_profit += City.profit
         City.save()
-        print("Save shield, progress, live level and profit to " + str(City.city_name))
+        print(str(country_name) + ": save shield, progress, live level and profit to " + str(City.city_name))
     
 
     #up round ===========================================================================================
     
     if Country.Round<7:
         Country.Round += 1
-        print("Upgrade round +1")
+        print(str(country_name) + ": upgrade round +1")
     else:
         Country.Round = 6
     
             
     #update ecology level ===================================================================================
     new_ecology = ecology.objects.get(round=Country.Round)
+    new_ecology.level = Ecology.level
     if ecology_dev == True:
         cost += 200
         if cost > Country.Budget:
             response_data = forms_check(request_data_show)
             return response_data
-        new_ecology.level = Ecology.level + 5
-        print("Upgrade ecology level")
+        new_ecology.level += 5
+        print(str(country_name) + ": upgrade ecology level")
         if new_ecology.level > 100:
             new_ecology.level = 100
     
             
 
     #set nuclear technology and order new rockets ==================================================
-    
-    if(nuclear_technology == True):
-        if nuclear_technology == True and Country.NuclearTechnology == False:
+    if nuclear_technology == True:
+        if Country.NuclearTechnology == False:
             cost += 500
             if cost > Country.Budget:
                 response_data = forms_check(request_data_show)
                 return response_data
             new_ecology.level -= 3
             Country.NuclearTechnology = nuclear_technology
-            print("Set nuclear technology on True")
+            print(str(country_name) + ": set nuclear technology on True")
         Country.NuclearRockets += nuclear_rockets
-        print("Upgrade order rockets on " + str(nuclear_rockets))
+        print(str(country_name) + ": upgrade order rockets on " + str(nuclear_rockets))
         cost += nuclear_rockets * 150
         if cost > Country.Budget:
             response_data = forms_check(request_data_show)
@@ -186,15 +181,15 @@ def calculations(request_data):
             
     #Budget ==========================================================================================
     Country.Budget += Country.Earnings - 20 * len(Sanctions_array) - cost
-    print("Calculate new country budget")
+    print(str(country_name) + ": calculate new country budget")
     Country.Earnings = sum_profit
-    print("Set new country earnings")
+    print(str(country_name) + ": set new country earnings")
                 
     Country.save()
-    print("General save country data")
+    print(str(country_name) + ": general save country data")
     Ecology.save()
     new_ecology.save()
-    print("Save ecology changes")
+    print(str(country_name) + ": save ecology changes")
     
     
     
@@ -211,7 +206,7 @@ def end_round(request):
 
         try:
             Country = country.objects.get(CountryName=country_name)
-            print("Find a country")
+            print("Find a country - " + str(country_name))
             response_data = calculations(request_data)
             return JsonResponse(response_data)
                
