410f2fc44733ab8b5f9a8dc4748114fce2504928
XanderJ312 <xanderj312@mail.ru>
2023-08-08 19:38:29 +0200

    Remake rocket attack scrypt

--- backend/round/views.py
+++ backend/round/views.py
@@ -5,6 +5,7 @@ from loginPage.models import country, city, ecology, sanction, user
 from .models import attacked_cities, session
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from timeout_decorator import timeout
 from donate.views import donate
 import json
 import jwt
@@ -19,28 +20,53 @@ def generate_jwt_token(payload):
 
 
 def forms_check(request_data_show):
-    print("Start forms_check")
+    country_name = request_data_show.get('country')
+    print(str(country_name) + ": start forms_check")
     Session = session.objects.get(id=1)
+    #all_sended = Session.sended
     while Session.forms_count <= Session.forms_max:
+        Session = session.objects.get(id=1)
+        if Session.sended == True:
+            if Session.forms_count == 1:
+                Session.sended = False
+                Session.save()
+                print("Set Session.sended = False")
+            Session.forms_count -= 1
+            Session.save()
+            print(str(country_name) + ": session.forms_count -1")
+            break
+        else:
+            if Session.forms_count == Session.forms_max:
+                Session.sended = True
+                print("Find a last form: " + str(country_name) + " (all forms were sended)")
+                response_data = requests.post('http://127.0.0.1:8000/attack', json=request_data_show)
+                print(str(country_name) + ": attack is successfull")
+                Session.forms_count -= 1
+                Session.save()
+                print(str(country_name) + ": session.forms_count -1")
+                break
+            else:
+                time.sleep(30)
+        '''
         Session = session.objects.get(id=1)
         if Session.forms_count == Session.forms_max:
             print("Find a last form")
             response_data = requests.post('http://127.0.0.1:8000/attack', json=request_data_show)
             Session.forms_count -= 1
-            print("Down Session.forms_count -1")
+            print(str(country_name) + ": down Session.forms_count -1")
             Session.save()
-            print("Save changes in session.forms_count")
+            print(str(country_name) + ": save changes in session.forms_count")
             json_response_data = response_data.json()
             return json_response_data
         else:
-            time.sleep(30)
+            time.sleep(10)
+        '''
     
     
 
 
 
 def calculations(request_data):
-    print(request_data)
     country_name = request_data.get('country')
     print(str(country_name) + ": start calculations")
     nuclear_technology = request_data.get('nuclear_technology')
@@ -64,18 +90,24 @@ def calculations(request_data):
     attacked_cities_list = []
     for enemy in enemies:
         for enemy_city in enemy['cities']:
-            if enemy_city['is_attacked'] == True:
+            if enemy_city['is_attacked'] == True and enemy_city['state'] == True:
+                Country.NuclearRockets -= 1
                 Ecology.level -= 2
+                attack_city = city.objects.filter(city_name=enemy_city['city_name']).values('id')
+                add_db_city = attacked_cities(city_name_id=attack_city[0]['id'])
+                add_db_city.save()
+                print(str(enemy_city['city_name']) + " was added in attacked_cities")
+                '''
                 attacked_city = {
                     'city_name': enemy_city['city_name']
                 }
                 attacked_cities_list.append(attacked_city)
+                '''
 
     request_data_show = {
         'country': country_name,
         'logincode': User.entercode,
-        'password': User.password,
-        'attacked_cities': attacked_cities_list
+        'password': User.password
     }
 
     cost = 0
@@ -106,24 +138,29 @@ def calculations(request_data):
         if one_request_city['shield'] == True and City.shield == False:
             cost += 300
             if cost > Country.Budget:
-                response_data = forms_check(request_data_show)
-                return response_data
+                response_data = requests.post('http://127.0.0.1:8000/login_page', json=request_data_show)
+                json_response_data = response_data.json()
+                return json_response_data
             City.shield = True
             print(str(country_name) + ": add shield for city " + str(City.city_name))
         if one_request_city['develop'] == True:
             cost += 150
             if cost > Country.Budget:
-                response_data = forms_check(request_data_show)
-                return response_data
+                response_data = requests.post('http://127.0.0.1:8000/login_page', json=request_data_show)
+                json_response_data = response_data.json()
+                return json_response_data
             City.progress += 8
             print(str(country_name) + ": upgrade progress to " + str(City.city_name))
             City.live_level = (Ecology.level * City.progress)/100
             print(str(country_name) + ": upgrade live level to " + str(City.city_name))
             City.profit = City.live_level*3
-            print(str(country_name) + "upgrade profit to " + str(City.city_name))
+            print(str(country_name) + " upgrade profit to " + str(City.city_name))
         sum_profit += City.profit
         City.save()
         print(str(country_name) + ": save shield, progress, live level and profit to " + str(City.city_name))
+
+    #attack cities of enemies ===========================================================================
+    forms_check(request_data_show)
     
 
     #up round ===========================================================================================
@@ -141,8 +178,9 @@ def calculations(request_data):
     if ecology_dev == True:
         cost += 200
         if cost > Country.Budget:
-            response_data = forms_check(request_data_show)
-            return response_data
+            response_data = requests.post('http://127.0.0.1:8000/login_page', json=request_data_show)
+            json_response_data = response_data.json()
+            return json_response_data
         new_ecology.level += 5
         print(str(country_name) + ": upgrade ecology level")
         if new_ecology.level > 100:
@@ -155,8 +193,9 @@ def calculations(request_data):
         if Country.NuclearTechnology == False:
             cost += 500
             if cost > Country.Budget:
-                response_data = forms_check(request_data_show)
-                return response_data
+                response_data = requests.post('http://127.0.0.1:8000/login_page', json=request_data_show)
+                json_response_data = response_data.json()
+                return json_response_data
             new_ecology.level -= 3
             Country.NuclearTechnology = nuclear_technology
             print(str(country_name) + ": set nuclear technology on True")
@@ -164,8 +203,9 @@ def calculations(request_data):
         print(str(country_name) + ": upgrade order rockets on " + str(nuclear_rockets))
         cost += nuclear_rockets * 150
         if cost > Country.Budget:
-            response_data = forms_check(request_data_show)
-            return response_data
+            response_data = requests.post('http://127.0.0.1:8000/login_page', json=request_data_show)
+            json_response_data = response_data.json()
+            return json_response_data
                 
     elif(nuclear_technology==False):
         Country.NuclearRockets = 0
@@ -173,16 +213,14 @@ def calculations(request_data):
 
 
     #sanctions count =================================================================================
-    print("Message before sanctions")
     Sanction = sanction.objects.filter(sanctionFor_id=Country.id)
-    print("Message after sanctions")
     Sanctions_array = list(Sanction)
     
             
     #Budget ==========================================================================================
     Country.Budget += Country.Earnings - 20 * len(Sanctions_array) - cost
     print(str(country_name) + ": calculate new country budget")
-    Country.Earnings = sum_profit
+    #Country.Earnings = sum_profit
     print(str(country_name) + ": set new country earnings")
                 
     Country.save()
@@ -193,8 +231,11 @@ def calculations(request_data):
     
     
     
-    response_data = forms_check(request_data_show)
-    return response_data
+    #response_data = forms_check(request_data_show)
+
+    response_data = requests.post('http://127.0.0.1:8000/login_page', json=request_data_show)
+    json_response_data = response_data.json()
+    return json_response_data
 
 
 @csrf_exempt
